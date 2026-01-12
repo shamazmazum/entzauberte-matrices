3a53cafa03ca4370807d2de4e6ef29c9023227d5
Vasily Postnicov <shamaz.mazum@gmail.com>
2026-01-12 15:47:51 +0300

    Decreaze the amount of noise when reshaping

--- src/utility.lisp
+++ src/utility.lisp
@@ -3,8 +3,11 @@
 (declaim (inline reshape))
 (defun reshape (m shape)
   "Reshape an array."
-  (assert (= (reduce #'* shape)
-             (array-total-size m)))
+  (locally
+      ;; Decrease the amount of noise
+      (declare (optimize (speed 0)))
+    (assert (= (reduce #'* shape)
+               (array-total-size m))))
   (let ((result (make-array shape :element-type (array-element-type m))))
     (replace (array-storage-vector result)
              (array-storage-vector m))
@@ -13,8 +16,11 @@
 (declaim (inline reshape-unsafe))
 (defun reshape-unsafe (m shape)
   "Reshape an array. The new array will share the storage with the argument."
-  (assert (= (reduce #'* shape)
-             (array-total-size m)))
+  (locally
+      ;; Decrease the amount of noise
+      (declare (optimize (speed 0)))
+    (assert (= (reduce #'* shape)
+               (array-total-size m))))
   (make-array shape
               :element-type (array-element-type m)
               :displaced-to m))
